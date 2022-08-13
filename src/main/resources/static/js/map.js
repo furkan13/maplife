7f17cddfcc0b1c6d41ac64ea3566a55f52fb53e4
Yanguo Cao <caoy35@cardiff.ac.uk>
2022-08-13 15:11:56 +0100

    Made the customized circle and default marker

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,6 +1,8 @@
+let popupContent = document.getElementById("marker-popup")
+// let markers = document.getElementsByClassName("custom-div-icon")
+// display the map layer
 var map = L.map('map').fitWorld();
-map.locate({setView: true, maxZoom: 16});
-
+map.locate({setView: true, maxZoom: 16}); // ask for the current location
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
@@ -8,11 +10,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 function onLocationFound(e) {
     var radius = e.accuracy;
-
-    L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-    L.circle(e.latlng, radius).addTo(map);
+    L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+    L.circle(e.latlng,{radius:radius,color:'#de5b19'}).addTo(map);
 }
 function onLocationError(e) {
     alert(e.message);
@@ -20,7 +19,17 @@ function onLocationError(e) {
 map.on('locationerror', onLocationError);
 map.on('locationfound', onLocationFound);
 
-// var popup = L.popup();
+//customize the default marker
+var orangeIcon = new L.Icon({
+    iconUrl: '../../static/image/marker-icon-2x-orange.png',
+    shadowUrl: '../../static/image/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+})
+L.Marker.prototype.options.icon = orangeIcon
+
 // function onMapClick(e) {
 //     popup
 //         .setLatLng(e.latlng)
@@ -30,27 +39,25 @@ map.on('locationfound', onLocationFound);
 //
 // map.on('click', onMapClick);
 
-//customize the marker
-// var MapIcon = L.Icon.extend({
-//     options: {
-//         // shadowUrl: 'image/cat.png',
-//         iconSize: [60, 60],
-//         // shadowSize: [0, 0],
-//         iconAnchor: [30, 30],
-//         // shadowAnchor: [4, 62],
-//         popupAnchor: [0, -40]
-//     }
-// });
-// // var catIcon = new LeafIcon({iconUrl: 'image/cat.png'}),
-// //     birdIcon = new LeafIcon({iconUrl: 'image/birds.jfif'});
-// var catIcon = new MapIcon({iconUrl: '../../static/image/cat.png'}),
-//     birdIcon = new MapIcon({iconUrl: '../../static/image/birds.jfif'});
-// L.icon = function (options) {
-//     return new L.Icon(options);
-// };
-// L.marker([51.483396, -3.173728], {icon: catIcon}).addTo(map).bindPopup("I am a cat.");
-// L.marker([51.489566, -3.187325], {icon: birdIcon}).addTo(map).bindPopup("I am a bird.");
 
-var myIcon = L.divIcon({className: 'custom-div-icon'});
-L.marker([51.483396, -3.173728], {icon: myIcon}).addTo(map).bindPopup("I am a cat.");
+//define the custom icon
+var myIcon = L.divIcon({className: 'custom-div-icon',iconAnchor: [25, 25],popupAnchor: [3, -30],iconUrl:""});
+
+//retrieve the streaming data
+var data = [
+    {'event_title':'Cat Lover','event_description':'My life with cats','event_cover':'cat.png','event_icon':'cat.png',lat:51.483396,lng:-3.173728},
+    {'event_title':'Bird life','event_description':'Do you know the birds?','event_cover':'birds.jfif','event_icon':'birds.jfif',lat:51.480023,lng:-3.170290}
+]
+//put the marker on the map
+for (let i = 0;data.length>i;i++){
+    let eventCoverImg = "../../static/image/" + data[i].event_cover
+    // let eventIconImg = "../../static/image/" + data[i].event_icon
+    popupContent = "<h1>" + data[i].event_title + "<h1><img id='event-img' src=" + eventCoverImg + ">"
+    // markers[i].style.backgroundImage= "url('"+ eventIcon +"')"
+    // document.getElementsByClassName("custom-div-icon")[0].style.backgroundImage="url('"+ eventIcon +"')"
+    L.marker([data[i].lat,data[i].lng], {icon: myIcon}).addTo(map).bindPopup(popupContent);
+}
+// L.marker([51.483396, -3.173728], {icon: myIcon}).addTo(map).bindPopup(popupContent);
+// marker.bindPopup(popupContent).openPopup(popup);
+
 
