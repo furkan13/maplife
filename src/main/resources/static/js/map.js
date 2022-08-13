780edfa63c6c15af902b31a80d10bdd2a3b76f54
Yanguo Cao <caoy35@cardiff.ac.uk>
2022-08-13 19:30:42 +0100

    The markers can display refer to the host's user icon

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,5 +1,5 @@
 let popupContent = document.getElementById("marker-popup")
-// let markers = document.getElementsByClassName("custom-div-icon")
+
 // display the map layer
 var map = L.map('map').fitWorld();
 map.locate({setView: true, maxZoom: 16}); // ask for the current location
@@ -21,8 +21,8 @@ map.on('locationfound', onLocationFound);
 
 //customize the default marker
 var orangeIcon = new L.Icon({
-    iconUrl: '../../static/image/marker-icon-2x-orange.png',
-    shadowUrl: '../../static/image/marker-shadow.png',
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
@@ -41,20 +41,20 @@ L.Marker.prototype.options.icon = orangeIcon
 
 
 //define the custom icon
-var myIcon = L.divIcon({className: 'custom-div-icon',iconAnchor: [25, 25],popupAnchor: [3, -30],iconUrl:""});
+var myIcon = L.divIcon({className:'custom-div-icon',iconAnchor: [25, 25],popupAnchor: [3, -30]});
 
 //retrieve the streaming data
 var data = [
     {'event_title':'Cat Lover','event_description':'My life with cats','event_cover':'cat.png','event_icon':'cat.png',lat:51.483396,lng:-3.173728},
     {'event_title':'Bird life','event_description':'Do you know the birds?','event_cover':'birds.jfif','event_icon':'birds.jfif',lat:51.480023,lng:-3.170290}
 ]
+
 //put the marker on the map
 for (let i = 0;data.length>i;i++){
     let eventCoverImg = "../../static/image/" + data[i].event_cover
-    // let eventIconImg = "../../static/image/" + data[i].event_icon
+    let eventIconImg = "../../static/image/" + data[i].event_icon
     popupContent = "<h1>" + data[i].event_title + "<h1><img id='event-img' src=" + eventCoverImg + ">"
-    // markers[i].style.backgroundImage= "url('"+ eventIcon +"')"
-    // document.getElementsByClassName("custom-div-icon")[0].style.backgroundImage="url('"+ eventIcon +"')"
+    myIcon.options.html = '<img id="custom-div-icon" class="custom-div-icon" src=' + eventIconImg + ">"
     L.marker([data[i].lat,data[i].lng], {icon: myIcon}).addTo(map).bindPopup(popupContent);
 }
 // L.marker([51.483396, -3.173728], {icon: myIcon}).addTo(map).bindPopup(popupContent);
