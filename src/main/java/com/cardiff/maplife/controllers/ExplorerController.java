d95ecbd1fc85be5fd5a2379af8322b2719265a40
Furkan Workstation <falahi161212@bscse.uiu.ac.bd>
2022-08-14 22:44:00 +0100

    Explore cards not showing in html -Pair program with yunguo

---
+++ src/main/java/com/cardiff/maplife/controllers/ExplorerController.java
@@ -0,0 +1,34 @@
+package com.cardiff.maplife.controllers;
+
+import com.cardiff.maplife.entities.Event;
+import com.cardiff.maplife.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+
+@Controller
+public class ExplorerController {
+
+    @Autowired
+    EventService eventservice;
+
+
+    @RequestMapping("/explore")
+    public String showMapPage(Model model) {
+
+
+        List <Event> eventList=eventservice.eventRepository.findAll();
+       model.addAttribute("eventList",eventList);
+        return "Explore/explore";
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/cardiff/maplife/repositories/EventRepository.java
@@ -0,0 +1,21 @@
+package com.cardiff.maplife.repositories;
+
+import com.cardiff.maplife.entities.Event;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EventRepository extends CrudRepository<Event, Long> {
+    Event save(Event event);
+
+
+    public List<Event> findAll();
+
+
+
+}
+
