53d3fb90b3eef88acfd93c5a2c7868ab055d7c67
Furkan Workstation <falahi161212@bscse.uiu.ac.bd>
2022-08-17 23:09:40 +0100

    Cleaned the code and added the comments for more readability TO DO -write the testing

--- src/main/java/com/cardiff/maplife/controllers/ExplorerController.java
+++ src/main/java/com/cardiff/maplife/controllers/ExplorerController.java
@@ -17,20 +17,21 @@ public class ExplorerController {
     @Autowired
     EventService eventservice;
 
+    /* All the controller methods for explorer page*/
 
-    @GetMapping("/explore")
-    public String showMapPage(Model model) {
+    @GetMapping("/explore")                         // method for explore page
+    public String showMapPage(Model model) {           // Passing a Model as parameter so that add an event object to model and pass it in the webpage
 
 
-        List <Event> eventList=eventservice.eventRepository.findAll();
-        if(eventList.size()>3)
+        List <Event> eventList=eventservice.eventRepository.findAll();  // getting all the events from the database
+        if(eventList.size()>3)                                          // check if there are more than 3 events
         {
-            eventList.subList(3, eventList.size()).clear();
+            eventList.subList(3, eventList.size()).clear();             // remove all the other events from the list after index 3 so that only three events will be shown in page
         }
        model.addAttribute("eventList",eventList);
         return "Explore/explore";
     }
-    @GetMapping("/nearby")
+    @GetMapping("/nearby")                                          //method for showing all the nearby events in different page
     public String showNearBy(Model model) {
 
 
@@ -38,7 +39,7 @@ public class ExplorerController {
         model.addAttribute("eventList",eventList);
         return "Explore/nearby";
     }
-    @GetMapping("/trending")
+    @GetMapping("/trending")                                         //method for showing all the trending events in different page
     public String showTrending(Model model) {
 
 
