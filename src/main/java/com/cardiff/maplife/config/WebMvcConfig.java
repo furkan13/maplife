5cebc0a633c26739632ddab4e3f71486ceec14bc
Yanguo Cao <caoy35@cardiff.ac.uk>
2022-08-20 23:07:47 +0100

    resolve conflicts from main branch

--- src/main/java/com/cardiff/maplife/config/WebMvcConfig.java
+++ src/main/java/com/cardiff/maplife/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.cardiff.maplife.config;
+package com.cardiff.maplife.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -10,9 +10,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     // Configure Virtual Path Mapping Access
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // images save location
-        registry.addResourceHandler("/image/**")
-                .addResourceLocations("file:C:\\Users\\c21086851\\OneDrive - Cardiff University\\dissertation\\project_44b_party_watch\\src\\main\\resources\\static\\image\\");
-
+        // images save location                 //static location for test, will be replaced by Furkan's
+//        registry.addResourceHandler("/image/**")
+//                .addResourceLocations("file:C:\\Users\\c21086851\\OneDrive - Cardiff University\\dissertation\\project_44b_party_watch\\src\\main\\resources\\static\\image\\");
     }
 }
