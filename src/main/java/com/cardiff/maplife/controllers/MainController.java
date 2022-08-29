80ed37121f19cb82d1c3d903b1a779d947d90903
Yanguo Cao <caoy35@cardiff.ac.uk>
2022-08-29 05:19:42 +0100

    Show recommendation and subscription dynamically on the sidebar

--- src/main/java/com/cardiff/maplife/controllers/MainController.java
+++ src/main/java/com/cardiff/maplife/controllers/MainController.java
@@ -1,25 +1,49 @@
 package com.cardiff.maplife.controllers;
 
 
+import com.cardiff.maplife.entities.Event;
 import com.cardiff.maplife.entities.User;
+import com.cardiff.maplife.services.EventService;
 import com.cardiff.maplife.services.UserService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 public class MainController {
     private final UserService userService;
+    private final EventService eventService;
 
-    public MainController(UserService userService) {
+    public MainController(UserService userService,EventService eventService) {
         this.userService = userService;
+        this.eventService = eventService;
+
     }
 
     @GetMapping("/")
     public ModelAndView showMapPage(ModelAndView modelAndView, HttpServletResponse response) {
         modelAndView = new ModelAndView("landing/map");
+        if (userService.getAuthentication()!=null){
+            List<Event> newEventList = new ArrayList<>();
+            User loggedUser = userService.findUserByUsername(userService.getAuthentication());
+            Set<User> followingUserSet = loggedUser.getFollowingUserSet();
+            Timestamp datetime = new Timestamp(System.currentTimeMillis());
+            List<Event> eventList = eventService.findCustom(datetime);
+            for (User followingUser : followingUserSet){
+                for (Event event : eventList){
+                    if (event.getUser().getUser_id().equals(followingUser.getUser_id())){
+                        newEventList.add(event);
+                    }
+                }
+            }
+            modelAndView.addObject("eventList",newEventList);
+        }
         return modelAndView;
     }
 
