d1cfba91a7c0ab6bdd0d31c0a8e83f9ec8b94d05
Furkan Workstation <falahi161212@bscse.uiu.ac.bd>
2022-09-06 16:40:54 +0100

    static ip added v6

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -5,7 +5,7 @@ variable "name1" { default = "maplife" }
 variable "keypair" { default = "furkan" } # you may need to change this
 variable "network" { default = "furkan_network" }   # you need to change this
 
-variable "pool" { default = "cscloud_private_floating" }
+variable "pool" { default = "furkan_network" }
 variable "server1_script" { default = "./furkanserver.sh" }
 variable "security_description" { default = "Terraform security group" }
 variable "security_name" { default = "tf_maplife" }
