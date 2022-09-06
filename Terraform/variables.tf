b09d04d471e9adb754121a5cc554bf4d658f7a9f
Furkan Workstation <falahi161212@bscse.uiu.ac.bd>
2022-09-06 15:24:39 +0100

    Terraform name changed

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "flavor" { default = "m1.large" }
 variable "image" { default = "Debian Buster 10.11.1 20211029" }
-variable "name1" { default = "desk_booking" }
+variable "name1" { default = "maplife" }
 
 variable "keypair" { default = "furkan" } # you may need to change this
 variable "network" { default = "furkan_network" }   # you need to change this
